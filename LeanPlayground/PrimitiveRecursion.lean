e021ade3fc1bf53679c8f3c0faa73163c1772712
Andrew Shulaev <ash.drone@gmail.com>
2024-08-16 00:58:05 +0100

    One more thing that does not work

--- LeanPlayground/PrimitiveRecursion.lean
+++ LeanPlayground/PrimitiveRecursion.lean
@@ -7,6 +7,12 @@ inductive T : Nat → Type
   -- First the number of iteration, then 0th element, then successor
   | iter : {k : Nat} → T k → T k → T (k + 1) → T k
 
+inductive PR : Nat → Type
+  | const : (n k : Nat) → PR k
+  | proj : {k : Nat} → (i : Fin k) → PR k
+  | comp : {m k : Nat} → PR m → Mathlib.Vector (PR k) m → PR k
+  | iter : {k : Nat} → PR k → PR (k + 2) → PR (k + 1)
+
 def eval {k : Nat} (env : Mathlib.Vector ℕ k) : T k → ℕ
   | T.var v => env.get v
   | T.const n _ => n
